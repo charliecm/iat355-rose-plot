f886216d54510953531640fdc83ff91b27c6226a
Charlie Chao <charliechao@outlook.com>
2017-02-07 19:05:03 -0800

    Scaffold layout and script.

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,61 @@
  * Main Interactions
  */
 
-$(function() {
+ document.addEventListener('DOMContentLoaded', function(e) {
 
-	console.log('Hello!');
+	var canvas = document.getElementById('canvas'),
+		svg = d3.select(canvas).append('svg');
+
+	function redraw() {
+		svg
+			.attrs({
+				width: canvas.clientWidth,
+				height: 300
+			})
+			.selectAll('rect').remove();
+		svg.append('rect').attrs({
+				x: 30,
+				y: 30,
+				width: 30,
+				height: 30,
+				color: 'red'
+			});
+	}
+
+	// https://github.com/jashkenas/underscore/blob/master/underscore.js#L880
+	function debounce(func, wait, immediate) {
+		var timeout, args, context, timestamp, result;
+		var _now = Date.now || function() {
+			return new Date().getTime();
+		};
+		var later = function() {
+			var last = _now() - timestamp;
+			if (last < wait && last >= 0) {
+				timeout = setTimeout(later, wait - last);
+			} else {
+				timeout = null;
+				if (!immediate) {
+					result = func.apply(context, args);
+					if (!timeout) context = args = null;
+				}
+			}
+		};
+		return function() {
+			context = this;
+			args = arguments;
+			timestamp = _now();
+			var callNow = immediate && !timeout;
+			if (!timeout) timeout = setTimeout(later, wait);
+			if (callNow) {
+				result = func.apply(context, args);
+				context = args = null;
+			}
+			return result;
+		};
+	};
+
+	// Initialize
+	window.addEventListener('resize', debounce(redraw, 500));
+	redraw();
 
 });
